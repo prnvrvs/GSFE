9b97bd90a5eefe6c442861645d30116396bb1ed7
prnvrvs <71942832+prnvrvs@users.noreply.github.com>
2022-01-07 10:15:17 +0530

    Update gsfe.sh

--- gsfe.sh
+++ gsfe.sh
@@ -4,9 +4,9 @@
 # atomsk web page :https://atomsk.univ-lille.fr/
 # run in interactive session  "bash -i gsfe.sh"
 
-
-module load gcc-9.1.0 # load necessary module to run atomsk
-for j in `seq -w 0.00 0.10 0.50`
+module load gcc-9.1.0
+box_height=20 #set box height in Z to include vacuum
+for j in `seq -w 0.00 0.10 0.00`
 do
 mkdir $j
 cd $j
@@ -14,7 +14,7 @@ for i in `seq -w 0.00 0.10 0.50`
 do
 mkdir $i
 cd $i
-atomsk ../../CONTCAR -shift above 0.48*BOX z $i*BOX $j*BOX 0.0 -fix z above 0.48*BOX z -cell add 6 H3 vasp
+atomsk ../../CONTCAR -shift above 0.48*BOX z $i*BOX $j*BOX 0.0 -fix z below 0.48*BOX z -cell set $box_height H3 vasp
 ln -s ../../INCAR INCAR
 ln -s ../../POTCAR POTCAR
 ln -s ../../KPOINTS KPOINTS
